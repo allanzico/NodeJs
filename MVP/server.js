5e36f91bd265012b16038f0c60cbd28c71548e23
Allan <akanyijuka@gmail.com>
2018-07-23 19:56:26 +0200

    Complete Project

--- MVP/server.js
+++ MVP/server.js
@@ -7,13 +7,15 @@ const morgan = require ('morgan');
 const request = require ('request');
 const async = require ('async');
 const expressHbs = require ('express-handlebars');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require ('express-flash');
 
 //Instantiate the Express application and use all the methods that Express has
 const app = express();
 
 
 //
-//' 
 
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', 'hbs');
@@ -21,11 +23,23 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false }));
 app.use (morgan('dev'));
+app.use(session({
+
+    resave: true,
+    saveUinitialized: true,
+    secret: 'secret',
+    store: new MongoStore ({url: 'mongodb://root:%40P455word@ds247061.mlab.com:47061/akanyijukanewsletter'})
+
+}));
+
+app.use(flash());
+
+//connect and use mongodb
 
 //Build a URL with request, respond and a callback
 app.route('/')
 .get((req, res, next) =>{
-res.render('main/home');
+res.render('main/home', {message: req.flash('success') });
 })
 .post((req, res, next) =>{
     //capture user's email
@@ -46,7 +60,7 @@ json: {
       if (err){
           console.log(err);
       } else {
-          console.log("Successfully sent");
+          req.flash('success', 'You have submitted your email');
           res.redirect('/');
       }
   });
